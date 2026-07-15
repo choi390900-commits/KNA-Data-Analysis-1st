5aed4c44c0c8fb73ed98cf583734725eb4c76fa7
choi390900-commit <choi390900@gmail.com>
2026-07-15 17:54:58 +0900

    test.py

---
+++ test.py
@@ -0,0 +1,73 @@
+count = 1 # int
+temp = 26.5 # float
+name = "Choi" # str
+me = True # bool
+
+# print(count, temp, name, me)
+print(count)
+print(temp)
+print(name)
+print(me)
+
+# ===================
+
+print(type(count))
+print(type(temp))
+print(type(name))
+print(type(me))
+
+# =========================
+
+print(type(100)) # <calss 'int'>
+print(type(100.0)) # <class 'fioat'>
+print(type("100")) # <class 'str'>
+
+# ===========================
+
+print(6 + 2) # 8
+print("6" + "2") # 62
+print("62" + "39") # 6239
+
+# =================
+print(3 > 2) # True
+print(5 == 5) # True
+print(type(3 > 2)) # <class 'bool'>
+
+# =====================
+count = 3
+print(count, type(count))
+
+count = 3.0
+print(count, type(count))
+
+count = "3"
+print(count, type(count))
+
+# ======================
+
+device_temp = 26.5 # floar
+check_count = 26 # int
+device_anme = "수업S" # str
+is_normal = True # bool
+
+# ========================
+# 연산자
+
+a = 17
+b = 5
+print(a + b) # 22
+print(a - b) # 12
+print(a * b) # 85
+print(a // b) # 3
+print(a % b) # 2
+print(a ** b) # 1419857
+
+# =====================
+
+x = 60
+y = 70
+z = 80
+print((x + y + z) / 3)
+side = 6
+print(side ** 2)
+print(3 * 4 * 5) 
